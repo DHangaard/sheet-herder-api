767ccf300961d78c6218b5d19a9ae658398c41d3
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-02-22 21:04:04 +0100

    Updated Main - added EntityManagerFactory in Singleton pattern to later inject in DAOs

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,17 @@
 package app;
 
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main
 {
+    // Singleton
+    private final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
     public static void main(String[] args)
     {
         System.out.println("hello, world");
+
+        emf.close();
     }
 }
