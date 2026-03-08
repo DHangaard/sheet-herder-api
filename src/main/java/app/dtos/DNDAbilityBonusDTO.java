7b625cba13270627e17b7334b9f416e9cf50738e
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-08 17:02:12 +0100

    Added DTO to map ability bonus which contains an ability score

---
+++ src/main/java/app/dtos/DNDAbilityBonusDTO.java
@@ -0,0 +1,15 @@
+package app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DNDAbilityBonusDTO(
+        @JsonProperty("ability_score")
+        DNDAbilityScoreDTO dndAbilityScoreDTO,
+
+        @JsonProperty("bonus")
+        int bonus
+)
+{
+}
