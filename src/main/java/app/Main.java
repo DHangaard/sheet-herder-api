659add9243419af8225a4a1f0ae2988ef27eac9b
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-08 17:17:16 +0100

    Instantiated DNDClient in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,29 @@
 package app;
 
+import app.integrations.DNDClient;
 import app.config.HibernateConfig;
+import app.utils.ExecutionTimer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.net.http.HttpClient;
 
 public class Main
 {
     // Singleton
-    private final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+    private final static EntityManagerFactory ENTITY_MANAGER_FACTORY = HibernateConfig.getEntityManagerFactory();
+    private final static HttpClient CLIENT = HttpClient.newHttpClient();
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static void main(String[] args)
     {
-        System.out.println("hello, world");
+        ExecutionTimer.start();
 
-        emf.close();
+        DNDClient dndClient = new DNDClient(CLIENT, OBJECT_MAPPER);
+        dndClient.getAllRaces().races().forEach(race -> System.out.println(race.name()));
+
+        ENTITY_MANAGER_FACTORY.close();
+
+        ExecutionTimer.finish();
     }
 }
