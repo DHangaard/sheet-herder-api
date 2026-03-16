cc247f827c2ca5c4f96fd0ca0d3271e8a9c50168
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-16 09:40:42 +0100

    Added RaceDTO (reference data) to project

---
+++ src/main/java/app/dtos/RaceDTO.java
@@ -0,0 +1,22 @@
+package app.dtos;
+
+import app.enums.Ability;
+import app.enums.Size;
+
+import java.util.Map;
+import java.util.Set;
+
+public record RaceDTO(
+        Long id,
+        String name,
+        int speed,
+        Map<Ability, Integer> abilityBonuses,
+        String ageDescription,
+        String alignment,
+        Size size,
+        String sizeDescription,
+        Set<LanguageDTO> languages,
+        Set<TraitDTO> traits
+)
+{
+}
