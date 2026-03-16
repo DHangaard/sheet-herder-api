f5a7049f04b7b548383a8d59c5901dd131128697
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-16 09:39:18 +0100

    Added Trait reference entity to project

---
+++ src/main/java/app/entities/reference/Trait.java
@@ -0,0 +1,55 @@
+package app.entities.reference;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Trait
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "trait_descriptions",
+            joinColumns = @JoinColumn(name = "trait_id")
+    )
+    @Column(name = "description", columnDefinition = "TEXT") // TODO Rethink use of "TEXT"
+    private List<String> descriptions; // TODO Check for instances of multiple descriptions or refactor
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Trait(String name, List<String> descriptions)
+    {
+        this.name = name;
+        this.descriptions = descriptions;
+    }
+
+    @PrePersist
+    protected void onCreate()
+    {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate()
+    {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
