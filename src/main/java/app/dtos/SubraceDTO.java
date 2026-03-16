744baf70e3be4eaded466435c384584e194b8687
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-16 09:40:51 +0100

    Added SubraceDTO (reference data) to project

---
+++ src/main/java/app/dtos/SubraceDTO.java
@@ -0,0 +1,17 @@
+package app.dtos;
+
+import app.enums.Ability;
+
+import java.util.Map;
+import java.util.Set;
+
+public record SubraceDTO(
+        Long id,
+        String name,
+        String description,
+        RaceDTO race,
+        Map<Ability, Integer> abilityBonuses,
+        Set<TraitDTO> traits
+)
+{
+}
