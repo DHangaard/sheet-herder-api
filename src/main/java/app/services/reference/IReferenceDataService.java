6832d6a28fdd754165222ac9f740235214f9a262
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-23 02:57:35 +0100

    Refactor IReferenceDataService to return Optional for getById and getByName methods, removed update and updateAll methods

--- src/main/java/app/services/reference/IReferenceDataService.java
+++ src/main/java/app/services/reference/IReferenceDataService.java
@@ -1,14 +1,13 @@
 package app.services.reference;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IReferenceDataService<T, R>
 {
     List<R> persistAll(List<T> dtos);
-    R getById(Long id);
-    R getByName(String name);
+    Optional<R> getById(Long id);
+    Optional<R> getByName(String name);
     List<R> getAll();
-    R update(T dto);
-    List<R> updateAll(List<T> dtos);
     Long delete(Long id);
 }
