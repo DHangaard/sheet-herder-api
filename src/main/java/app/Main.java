fc3b9f9b650c9fd699ceac81fe5fa3e39b303dad
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-23 03:17:25 +0100

    Refactor Main class: update imports, remove unused code, and start Javalin server

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,21 +1,26 @@
 package app;
 
-import app.dtos.LanguageDTO;
-import app.dtos.RaceDTO;
-import app.dtos.SubraceDTO;
-import app.dtos.TraitDTO;
+import app.config.ApplicationConfig;
+import app.dtos.reference.LanguageDTO;
+import app.dtos.reference.RaceDTO;
+import app.dtos.reference.SubraceDTO;
+import app.dtos.reference.TraitDTO;
 import app.dtos.dnd.DNDLanguageDetailDTO;
 import app.dtos.dnd.DNDRaceDetailDTO;
 import app.dtos.dnd.DNDSubraceDetailDTO;
 import app.dtos.dnd.DNDTraitDetailDTO;
-import app.entities.reference.Language;
-import app.entities.reference.Race;
-import app.entities.reference.Subrace;
-import app.entities.reference.Trait;
+import app.persistence.daos.implementations.LanguageDAO;
+import app.persistence.daos.implementations.RaceDAO;
+import app.persistence.daos.implementations.SubraceDAO;
+import app.persistence.daos.implementations.TraitDAO;
+import app.persistence.daos.interfaces.IReferenceDAO;
+import app.persistence.entities.reference.Language;
+import app.persistence.entities.reference.Race;
+import app.persistence.entities.reference.Subrace;
+import app.persistence.entities.reference.Trait;
 import app.integrations.DNDClient;
 import app.config.HibernateConfig;
 import app.integrations.IDNDClient;
-import app.persistence.*;
 import app.services.reference.*;
 import app.utils.ExecutionTimer;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -59,31 +64,6 @@ public class Main
         System.out.println("Races count: " + races.size());         // Should be 9
         System.out.println("Subraces count: " + subraces.size());   // Should be 4
 
-        /*
-        // TODO Remove
-        traits.forEach(t -> {
-            t.descriptions().forEach(d -> System.out.printf("Trait[%s] lengths -> description=%d%n", t.name(), len(d)));
-        });
-
-        races.forEach(r -> System.out.printf(
-                "Race[%s] lengths -> ageDescription=%d, alignment=%d, sizeDescription=%d, languageDescription=%d%n",
-                r.name(),
-                len(r.ageDescription()),
-                len(r.alignment()),
-                len(r.size()),
-                len(r.languageDescription())
-        ));
-
-         subraces.forEach(r -> System.out.printf(
-                "Subrace[%s] lengths -> description=%d, alignment=%d, sizeDescription=%d, languageDescription=%d%n",
-                r.name(),
-                len(r.ageDescription()),
-                len(r.alignment()),
-                len(r.size()),
-                len(r.languageDescription())
-        ));
-         */
-
         System.out.println("\nPersisting...\n");
 
         languageService.persistAll(languages);
@@ -91,22 +71,18 @@ public class Main
         raceService.persistAll(races);
         subraceService.persistAll(subraces);
 
+        ApplicationConfig.startServer(7070);
+
+        /*
         System.out.println("Languages from DB count: " + languageService.getAll().size());    // Should be 16
         System.out.println("Traits from DB count: " + traitService.getAll().size());       // Should be 38
         System.out.println("Races from DB count: " + raceService.getAll().size());         // Should be 9
         System.out.println("Subraces from DB count: " + subraceService.getAll().size());   // Should be 4
 
-        // dndFetchingService.fetchAllRaces().races().forEach(race -> System.out.println(race.name()));
-        // dndFetchingService.fetchAllLanguagesWithDetails().forEach(language -> System.out.println(language.typicalSpeakers()));
-        // dndFetchingService.fetchAllRacesWithDetails().forEach(race -> System.out.println(race.size()));
-
         ENTITY_MANAGER_FACTORY.close();
+         */
+
 
         System.out.println(ExecutionTimer.finish());
-    }
-
-    // TODO Remove
-    private static int len(String s) {
-        return s == null ?0 : s.length();
     }
 }
