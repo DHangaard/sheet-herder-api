04fca20ca0e2bb98c01f00987bc95bd857cc792d
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-31 01:52:46 +0200

    ApplicationConfig: Refactored -> separation of concerns, helper methods added. Security added to config.

--- src/main/java/app/config/ApplicationConfig.java
+++ src/main/java/app/config/ApplicationConfig.java
@@ -1,29 +1,27 @@
 package app.config;
 
+import app.exceptions.ApiException;
 import app.routes.*;
+import app.security.enums.Role;
+import app.security.routes.SecurityRoute;
 import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+
+import java.util.Map;
 
 public class ApplicationConfig
 {
     public static Javalin startServer(int port)
     {
         DIContainer diContainer = DIContainer.getInstance();
+        DataSeeder.seed(diContainer);
         Routes routes = buildRoutes(diContainer);
 
         Javalin app = Javalin.create(config ->
         {
-            config.bundledPlugins.enableRouteOverview("/routes");
-            config.routes.apiBuilder(routes.getRoutes());
-            config.routes.exception(RuntimeException.class, (e, ctx) ->
-            {
-                ctx.status(400).json(e.getMessage());
-            });
-            config.routes.exception(Exception.class, (e, ctx) ->
-            {
-               e.printStackTrace();
-               ctx.status(500).json(e.getMessage());
-            });
-
+            configureRoutes(config, routes);
+            configureSecurity(config, diContainer);
+            configureExceptions(config);
         }).start(port);
 
         return app;
@@ -40,8 +38,26 @@ public class ApplicationConfig
                 new LanguageRoute(diContainer.getLanguageController()),
                 new TraitRoute(diContainer.getTraitController()),
                 new RaceRoute(diContainer.getRaceController()),
-                new SubraceRoute(diContainer.getSubraceController())
+                new SubraceRoute(diContainer.getSubraceController()),
+                new SecurityRoute(diContainer.getSecurityController())
         );
     }
 
+    private static void configureRoutes(JavalinConfig config, Routes routes)
+    {
+        config.bundledPlugins.enableRouteOverview("/routes", Role.ANYONE);
+        config.routes.apiBuilder(routes.getRoutes());
+    }
+
+    private static void configureSecurity(JavalinConfig config, DIContainer diContainer)
+    {
+        config.routes.beforeMatched(diContainer.getSecurityController()::authenticate);
+        config.routes.beforeMatched(diContainer.getSecurityController()::authorize);
+    }
+
+    private static void configureExceptions(JavalinConfig config)
+    {
+        config.routes.exception(ApiException.class, (e, ctx) ->
+                ctx.status(e.getCode()).json(Map.of("status", e.getCode(), "message", e.getMessage())));
+    }
 }
