00439ed7d70a6adae214c3c2943beb9fc257fd09
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-04-03 02:48:59 +0200

    ICharacterSheetController: Interface and implementation added to project

---
+++ src/main/java/app/controllers/implementations/CharacterSheetController.java
@@ -0,0 +1,76 @@
+package app.controllers.implementations;
+
+import app.dtos.domain.CharacterSheetDTO;
+import app.dtos.domain.CreateCharacterSheetDTO;
+import app.dtos.domain.UpdateCharacterSheetDTO;
+import app.persistence.entities.domain.User;
+import app.security.dtos.UserDTO;
+import app.services.domain.interfaces.ICharacterSheetService;
+import app.services.domain.interfaces.IUserService;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+
+import java.util.List;
+
+public class CharacterSheetController implements app.controllers.interfaces.ICharacterSheetController
+{
+    private static final String USER_ATTRIBUTE = "user";
+    private final ICharacterSheetService characterSheetService;
+    private final IUserService userService;
+
+    public CharacterSheetController(ICharacterSheetService characterSheetService, IUserService userService)
+    {
+        this.characterSheetService = characterSheetService;
+        this.userService = userService;
+    }
+
+    @Override
+    public void create(Context ctx)
+    {
+        User user = resolveUser(ctx);
+        CreateCharacterSheetDTO createCharacterSheetDTO = ctx.bodyAsClass(CreateCharacterSheetDTO.class);
+        CharacterSheetDTO characterSheetDTO = characterSheetService.create(user, createCharacterSheetDTO);
+        ctx.status(HttpStatus.CREATED).json(characterSheetDTO);
+    }
+
+    @Override
+    public void getById(Context ctx)
+    {
+        User user = resolveUser(ctx);
+        Long id = Long.parseLong(ctx.pathParam("id"));
+        CharacterSheetDTO characterSheetDTO = characterSheetService.getById(user, id);
+        ctx.status(HttpStatus.OK).json(characterSheetDTO);
+    }
+
+    @Override
+    public void update(Context ctx)
+    {
+        User user = resolveUser(ctx);
+        UpdateCharacterSheetDTO updateCharacterSheetDTO = ctx.bodyAsClass(UpdateCharacterSheetDTO.class);
+        CharacterSheetDTO characterSheetDTO = characterSheetService.update(user, updateCharacterSheetDTO);
+        ctx.status(HttpStatus.OK).json(characterSheetDTO);
+    }
+
+    @Override
+    public void delete(Context ctx)
+    {
+        User user = resolveUser(ctx);
+        Long id = Long.parseLong(ctx.pathParam("id"));
+        characterSheetService.delete(user, id);
+        ctx.status(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public void findAllByUser(Context ctx)
+    {
+        User user = resolveUser(ctx);
+        List<CharacterSheetDTO> characterSheetDTOs = characterSheetService.findAllByUser(user);
+        ctx.status(HttpStatus.OK).json(characterSheetDTOs);
+    }
+
+    private User resolveUser(Context ctx)
+    {
+        UserDTO userDTO = ctx.attribute(USER_ATTRIBUTE);
+        return userService.getByUsername(userDTO.username());
+    }
+}
