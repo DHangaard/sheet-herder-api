79aa80991afe581f20202da9191b4f48177ae8c1
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-04-03 21:42:09 +0200

    UserRoute added to project

---
+++ src/main/java/app/routes/UserRoute.java
@@ -0,0 +1,26 @@
+package app.routes;
+
+import app.controllers.interfaces.IUserController;
+import app.security.enums.Role;
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class UserRoute
+{
+    private final IUserController userController;
+
+    public UserRoute(IUserController userController)
+    {
+        this.userController = userController;
+    }
+
+    public EndpointGroup getRoutes()
+    {
+        return () -> path("/users", () ->
+        {
+            put("/{id}", userController::update, Role.USER);
+            delete("/{id}", userController::delete, Role.USER);
+        });
+    }
+}
