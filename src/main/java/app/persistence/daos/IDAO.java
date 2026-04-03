4ee89bb098582a4986d3165098657bdfab815e30
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-04-03 02:41:46 +0200

    IDAO: Removed getAll for cleaner design

--- src/main/java/app/persistence/daos/IDAO.java
+++ src/main/java/app/persistence/daos/IDAO.java
@@ -1,13 +1,9 @@
-package app.persistence.daos.interfaces;
-
-import java.util.List;
-
+package app.persistence.daos;
 
 public interface IDAO <T>
 {
     T create(T t);
     T getById(Long id);
-    List<T> getAll();
     T update(T t);
     Long delete(Long id);
 }
