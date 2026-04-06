d5cc955f00818ce9b14dde2ec30476f15eb7a2c2
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-04-06 23:28:35 +0200

    feat: add HealthCheckRoute for health check endpoint

---
+++ src/main/java/app/routes/HealthCheckRoute.java
@@ -0,0 +1,22 @@
+package app.routes;
+
+import app.controllers.interfaces.IHealthCheckController;
+import app.security.enums.Role;
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class HealthCheckRoute
+{
+    private final IHealthCheckController healthCheckController;
+
+    public HealthCheckRoute(IHealthCheckController healthCheckController)
+    {
+        this.healthCheckController = healthCheckController;
+    }
+
+    public EndpointGroup getRoutes()
+    {
+        return () -> get("/health-check", healthCheckController::healthCheck, Role.ANYONE);
+    }
+}
