8e193913c436961cef2e4412321f2aeb496d027f
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-04-10 23:02:33 +0200

    refactor(HealthCheckController): make EntityManagerFactory field final

--- src/main/java/app/controllers/implementations/HealthCheckController.java
+++ src/main/java/app/controllers/implementations/HealthCheckController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HealthCheckController implements IHealthCheckController
 {
-    EntityManagerFactory emf;
+    private final EntityManagerFactory emf;
 
     public HealthCheckController(EntityManagerFactory emf)
     {
